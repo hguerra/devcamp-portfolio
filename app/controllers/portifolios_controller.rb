19b5d40a8b196c99c5ec765da19b7c067e43b8f5
Heitor Carneiro <heitorgcarneiro@gmail.com>
2018-02-12 09:56:56 -0200

    Adding edit method

--- app/controllers/portifolios_controller.rb
+++ app/controllers/portifolios_controller.rb
@@ -18,4 +18,20 @@ class PortifoliosController < ApplicationController
       end
     end
   end
+
+  def edit
+    @portfolio_item = Portifolio.find(params[:id])
+  end
+
+  def update
+    @portfolio_item = Portifolio.find(params[:id])
+
+    respond_to do |format|
+      if @portfolio_item.update(params.require(:portifolio).permit(:title, :subtitle, :body))
+        format.html { redirect_to portifolios_path, notice: 'Portfolio was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
 end
